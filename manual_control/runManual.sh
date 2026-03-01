d3b2be39a275d2ec76a5d607e55063b36cc3f37c
Radin <radin44@protonmail.com>
2026-02-28 16:23:54 -0800

    changed shell scripts from taking user input to automatically assigning serial port

--- manual_control/runManual.sh
+++ manual_control/runManual.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# ask the user for their Arduino Mega board
-read -p "Enter your Arduino Mega Port (e.g., /dev/ttyACM0): " PORT
-
 # compile the code with 
 arduino-cli compile --fqbn arduino:avr:mega --build-property "compiler.cpp.extra_flags=-Iinclude" .
 
 # upload the code to the board
-arduino-cli upload -p $PORT --fqbn arduino:avr:mega
+arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:mega
 
 # open the Serial Monitor at baudrate of 115200 for user input
-arduino-cli monitor -p $PORT --config baudrate=115200
+arduino-cli monitor -p /dev/ttyACM0 --config baudrate=115200
